0658eb461bc391d406e38a60989fac9ae84cd5a6
Nicholas Trimble <nicktrimble07@gmail.com>
2025-11-23 21:36:58 -0600

    pushing ETL setup

--- src/etl.py
+++ src/etl.py
@@ -9,3 +9,11 @@ client = tweepy.Client(bearer_token=bearer_token)
 
 query = "Python programming is:retweet"
 tweets = client.search_tweets(query=query, lang="en", max_results=100)
+
+
+data = []
+for tweet in tweets.data:
+    data.append({"text": tweet.text, "id": tweet.id})
+
+df = pd.DataFrame(data)
+df.to_csv("data/X_data.csv", index=False)
